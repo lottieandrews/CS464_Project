5862aa073b41f61164dc1a034c072c8486a9bc84
Lottie Andrews <lottieandrews10@gmail.com>
2021-04-13 19:36:07 -0600

    Add File.java

---
+++ src/filesys/File.java
@@ -0,0 +1,23 @@
+package filesys;
+
+public class File extends Node {
+
+    private String fileText = "";
+
+    public File(String name) {
+        super(name);
+    }
+
+    public File(String name, String fileText) {
+        super(name);
+        this.fileText = fileText;
+    }
+
+    public String getFileText() {
+        return fileText;
+    }
+
+    public void setFileText(String fileText) {
+        this.fileText = fileText;
+    }
+}
