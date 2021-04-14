3cb048e632e99ee96a128f3bbdbb3d7924e50990
Lottie Andrews <lottieandrews10@gmail.com>
2021-04-14 15:53:31 -0600

    Write less()

--- src/filesys/FileNavigator.java
+++ src/filesys/FileNavigator.java
@@ -15,8 +15,13 @@ public class FileNavigator {
         }
     }
 
-    public static void less(File file) {
-        System.out.println(file.getFileText());
+    public static void less(String fileName) {
+        if (currentDir.getChildren().get(fileName) != null) {
+            System.out.println(currentDir.getChildren().get(fileName).getFileText());
+        }
+        else {
+            errorHandler(new NullPointerException(fileName + ": No such file or directory"));
+        }
     }
 
     private static void errorHandler(Exception e) {
@@ -25,7 +30,7 @@ public class FileNavigator {
 
     public static void main(String[] args) {
         ROOT_DIR.setName("Start");
-        pwd();
+        //pwd();
         Directory folder1 = new Directory("Folder1");
         Directory folder2 = new Directory("Folder2");
         Directory folder3 = new Directory("Folder3");
@@ -35,12 +40,8 @@ public class FileNavigator {
         File file1 = new File("File1", "This is a file.");
         file1.setFileText("This is a file. It contains text.");
         ROOT_DIR.addChild(file1);
-        ls();
-        try {
-            file1.getChildren();
-        } catch (Exception e) {
-            errorHandler(e);
-        }
+        //ls();
+        less("File1");
         //System.out.println((ROOT_DIR.getChildren().get("File1")).getFileText());
         //System.out.println(folder.getParentName() + "/" + folder.getName());
 
