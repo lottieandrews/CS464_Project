eb171c3135e875a8597cac7a0462aa015fecb82f
Lottie Andrews <lottieandrews10@gmail.com>
2021-04-30 15:50:41 -0600

    Unify filename casing convention

--- src/game/Clue.java
+++ src/game/Clue.java
@@ -6,32 +6,32 @@ public class Clue {
         public File confidential = new File("CONFIDENTIAL", "Colonel Mustard in the study with the revolver!");
         public Directory rooms = new Directory("Rooms");
             public Directory study = new Directory("Study");
-                public File studyClue1 = new File("StudyClue1", "You have to believe me! It wasn't Miss Scarlet!");
-                public File studyClue2 = new File("StudyClue2", "The wrench wasn't the murder weapon! I swear!");
+                public File studyClue1 = new File("studyClue1", "You have to believe me! It wasn't Miss Scarlet!");
+                public File studyClue2 = new File("studyClue2", "The wrench wasn't the murder weapon! I swear!");
             public Directory kitchen = new Directory("Kitchen");
-                public File kitchenClue1 = new File("KitchenClue1", "I've been with Reverend Green all night! It couldn't have been him!");
-                public File kitchenClue2 = new File("KitchenClue2", "No way it happened in the Library!");
+                public File kitchenClue1 = new File("kitchenClue1", "I've been with Reverend Green all night! It couldn't have been him!");
+                public File kitchenClue2 = new File("kitchenClue2", "No way it happened in the Library!");
             public Directory hall = new Directory("Hall");
-                public File hallClue1 = new File("HallClue1", "I don't know who dunnit but it wasn't Professor Plum!");
-                public File hallClue2 = new File("HallClue2", "Not sure where it happened but it definitely wasn't in the Billiard Room!");
+                public File hallClue1 = new File("hallClue1", "I don't know who dunnit but it wasn't Professor Plum!");
+                public File hallClue2 = new File("hallClue2", "Not sure where it happened but it definitely wasn't in the Billiard Room!");
             public Directory conservatory = new Directory("Conservatory");
-                public File conservatoryClue1 = new File("ConservatoryClue1", "It didn't happen in the Lounge now leave me alone!");
-                public File conservatoryClue2 = new File("ConservatoryClue2", "A murder using a candlestick? Can you imagine? Definitely not!");
+                public File conservatoryClue1 = new File("conservatoryClue1", "It didn't happen in the Lounge now leave me alone!");
+                public File conservatoryClue2 = new File("conservatoryClue2", "A murder using a candlestick? Can you imagine? Definitely not!");
             public Directory lounge = new Directory("Lounge");
-                public File loungeClue1 = new File("LoungeClue1", "Mrs. White wouldn't hurt a fly! It most certainly wasn't her!");
-                public File loungeClue2 = new File("LoungeClue2", "Don't know where it happened but it definitely wasn't in the conservatory!");
+                public File loungeClue1 = new File("loungeClue1", "Mrs. White wouldn't hurt a fly! It most certainly wasn't her!");
+                public File loungeClue2 = new File("loungeClue2", "Don't know where it happened but it definitely wasn't in the conservatory!");
             public Directory ballroom = new Directory("Ballroom");
-                public File ballroomClue1 = new File("BallroomClue1", "It wasn't in the dining room! Couldn't have been!");
-                public File ballroomClue2 = new File("BallroomClue2", "No way he was killed with a knife!");
+                public File ballroomClue1 = new File("ballroomClue1", "It wasn't in the dining room! Couldn't have been!");
+                public File ballroomClue2 = new File("ballroomClue2", "No way he was killed with a knife!");
             public Directory diningRoom = new Directory("Dining_Room");
-                public File diningClue1 = new File("DiningClue1", "Don't ask me how I know this but it wasn't Mrs. Peacock!");
-                public File diningClue2 = new File("DiningClue2", "Ballroom's too clean for it to have happened in there!");
+                public File diningClue1 = new File("diningClue1", "Don't ask me how I know this but it wasn't Mrs. Peacock!");
+                public File diningClue2 = new File("diningClue2", "Ballroom's too clean for it to have happened in there!");
             public Directory library = new Directory("Library");
-                public File libraryClue1 = new File("LibraryClue1", "Definitely didn't happen in the Kitchen!");
-                public File libraryClue2 = new File("LibraryClue2", "All the plumbing seems to be intact so it couldn't have been the lead pipe!");
+                public File libraryClue1 = new File("libraryClue1", "Definitely didn't happen in the Kitchen!");
+                public File libraryClue2 = new File("libraryClue2", "All the plumbing seems to be intact so it couldn't have been the lead pipe!");
             public Directory billiardRoom = new Directory("Billiard_Room");
-                public File billiardClue1 = new File("BilliardClue1", "It most certainly couldn't have happened in the hall!");
-                public File billiardClue2 = new File("BilliardClue2", "Seriously? Obviously the murder weapon wasn't the rope!");
+                public File billiardClue1 = new File("billiardClue1", "It most certainly couldn't have happened in the hall!");
+                public File billiardClue2 = new File("billiardClue2", "Seriously? Obviously the murder weapon wasn't the rope!");
 
     public Clue() {
         clue.addChild(confidential);
@@ -66,12 +66,12 @@ public class Clue {
     }
 
     public static void main(String[] args){
-        System.out.println("Welcome to Clue the Terminal Game!");
-        System.out.println("Use the cd, exit, grep, ls, mkdir, more, mv, pwd, and rm commands to " +
+        System.out.println("\nWelcome to Bash Detective: The Clue-Inspired Terminal Game!");
+        System.out.println("\nUse the cd, exit, grep, ls, mkdir, more, mv, pwd, and rm commands to " +
         "investigate the clues hidden in each room and find out who dunnit, where they dunnit, and what they dunnit with! " + 
         "Feel free to use the man command if you get stuck on how to properly use a command and need some help. Use the " +
         "exit command when you're finished to exit the game.");
-        System.out.println("Start by creating a directory called 'Notebook' to store your clues.");
+        System.out.println("\nStart by creating a directory called 'Notebook' to store your clues.");
         Clue clue = new Clue();
         Terminal tr = new Terminal(clue.clue);
         tr.launch();
